512778ba5d5c0de9748733fadcfa63d6227653a6
Nguyen Phat <phatnt.uit@gmail.com>
2021-07-16 22:50:47 +0700

    feat: add weather details

--- weather-details/src/app/weatherDetails/weatherDetails.component.ts
+++ weather-details/src/app/weatherDetails/weatherDetails.component.ts
@@ -1,16 +1,22 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'weather-details',
-  templateUrl: './weatherDetails.component.html',
-  styleUrls: ['./weatherDetails.component.scss']
+  selector: "weather-details",
+  templateUrl: "./weatherDetails.component.html",
+  styleUrls: ["./weatherDetails.component.scss"],
 })
-
 export class WeatherDetails implements OnInit {
   @Input() weatherData: data[];
 
-  ngOnInit() {
+  searchTerm = "";
+  searchedData?: data = undefined;
 
+  ngOnInit() {}
+
+  onChange(value: string | null) {
+    this.searchedData = this.weatherData.find(
+      ({ name }) => name.toLowerCase() === value.toLowerCase()
+    );
   }
 }
 
